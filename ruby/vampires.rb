6fce693335561f25d57fde76c36c80924c1e5785
Kristina Vragovic <k.vragovic@gmail.com>
2017-04-22 15:15:01 -0500

    Print questions and retrieve answers from user

---
+++ ruby/vampires.rb
@@ -0,0 +1,26 @@
+# DBC 4.4
+# Interview with a Vampire
+
+# Ask a new employee the following questions: 
+# What is your name? 
+# How old are you? 
+# What year were you born? 
+# Our company cafeteria serves garlic bread. Would you like us to order some for you? 
+# Would you like to enroll in the company's health insurance?
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+alleged_age = gets.chomp
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "Our cafeteria serves garlic bread. Would you like us to order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
+
+
