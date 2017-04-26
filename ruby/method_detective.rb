11875c2cf6a5c5b71beaa0f6b10b972077f19b10
Kristina Vragovic <k.vragovic@gmail.com>
2017-04-25 21:39:51 -0500

    Catch em all

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,41 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase # Creates a new string
+"iNvEsTiGaTiOn".swapcase! # Overwrites the original string (!)
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(2, "o") 
+"zom".replace "zoom" 
+"zom".gsub(/o/, "oo") 
+"zom".gsub!(/o/, "oo") 
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase # or upcase!
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".insert(-1, " suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".insert(0, "the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chomp("r")
+"The case of the disappearing last letter".chop 
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".delete "T"
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# 122 is the 'integer ordinal' of z - its position on the ASCII table. Whatever that means!
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
