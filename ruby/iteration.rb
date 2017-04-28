9513e1b3268f7015bf6365c3397499ceb26aa8fd
Kristina Vragovic <k.vragovic@gmail.com>
2017-04-27 20:53:02 -0500

    Add array and hash with .each and .map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,5 @@
+# Release 0
+
 def robot(arms, legs)
   limbs = arms + legs
   puts limbs
@@ -5,3 +7,31 @@ def robot(arms, legs)
 end
 
 robot(12, 24) { puts "That's a lot of limbs!" } 
+
+
+# Release 1
+
+age_of_robots = [1, 25, 13, 43, 5, 610]
+
+robot_features = {
+  :eyes => 2,
+  :mouths => 3,
+  :arms => 12,
+  :legs => 24,
+  :buttons => 100
+}
+
+age_of_robots.each do |age|
+  puts "This robot is #{age} today!"
+end
+
+robot_features.each do |feature, number|
+  puts "This robot has a whopping #{number} #{feature}."
+end
+
+age_of_robots.map! do |age|
+  puts age
+  age.next 
+end
+
+p age_of_robots 
