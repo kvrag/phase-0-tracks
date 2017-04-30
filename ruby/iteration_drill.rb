c630c7a687d94cc3c513717f6e38f2ec61a273c1
Kristina Vragovic <k.vragovic@gmail.com>
2017-04-30 18:13:28 -0500

    Complete first array drill

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -6,15 +6,36 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # 1. Iterate through the zombie_apocalypse_supplies array,
 # printing each item in the array separated by an asterisk
 # ----
+zombie_apocalypse_supplies.each do |supply|
+  puts "#{supply}"
+  puts "*"
+end
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
 # ----
 
+
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
 # ----
+def supply_search(item_query)
+  zombie_apocalypse_supplies.each do |supply|
+    if supply == item_query
+      p "Yes, #{item_query} is in supply."
+    else
+      p "Nope - better stock up on #{item_query}."
+    end
+  end
+end
+
+puts "What item are you checking for?"
+item = gets.chomp
+puts supply_search(item)
+
+
+
 
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
