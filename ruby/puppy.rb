911e19e8a6b4c9bad6d6c40751c325a71c71a853
Kristina Vragovic <k.vragovic@gmail.com>
2017-05-02 11:44:44 -0500

    Describe Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+=begin 
+
+CLASS -------------------
+Puppy
+
+CHARACTERISTICS ---------
+Furry: yes
+Legs: 4
+Nose: wet
+Toes: 16
+Eyes: brown
+
+BEHAVIOR ----------------
+Wag tail
+Lick
+Run
+Sleep
+
+=end
