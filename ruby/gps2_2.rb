76273bd75f2b15b2324db41ebde3e78981fd1bef
Kristina Vragovic <k.vragovic@gmail.com>
2017-05-01 22:39:40 -0500

    Reflect on concepts in end comments

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -94,3 +94,40 @@ end
 print_finished_list(grocery_list)
 puts "Whew!" 
 
+=begin 
+
+What did you learn about pseudocode from working on this challenge?
+--
+Pseudocode is difficult to write with someone else! When I'm analyzing and deconstructing, I tend to get very granular, and others might not so much. It's also very important to set expectations beforehand.
+
+
+What are the tradeoffs of using arrays and hashes for this challenge?
+--
+Using an array at first, just to gather the first few items, was helpful for getting the list started. I can see how continuing to use arrays might help the user interface be less confusing / circular - you can just say "add an item and quantity" and return that right back. With a hash, you have to mess with keys and values - though it prints a very nice list in the end. :) 
+
+
+What does a method return?
+--
+A method returns the last evaluated expression in the method. That's why each method has 'grocery_list' at the end, to make sure the method is returning the finished list.
+
+
+What kind of things can you pass into methods as arguments?
+--
+Any kind of primitive data can be passed into the method as an argument, but you can also pass a data structure like a hash or an array.
+
+
+How can you pass information between methods?
+--
+This is the part that I didn't quite get this week. Perhaps we did not approach this challenge in a way that made passing information between methods necessary. However, it seems like establishing global variables to pass information back and forth is necessary. You can also call a method within a method if necessary.
+
+
+What concepts were solidified in this challenge, and what concepts are still confusing?
+--
+I definitely feel much more confident building out hashes, adding new items and removing them, etc, and creating loops in the user interface to account for what the user might need to do (change the quantity to zero, for instance, thereby deleting the item from the list). "Passing information between methods" - I'm still not sure if we even did that.
+
+=end 
+
+
+
+
+
