1f8b05d52e4932dc19cd4bf9015b746129308939
Kristina Vragovic <k.vragovic@gmail.com>
2017-05-07 18:02:10 -0500

    Convert Shout module to mixin and create Sibling and Coworker classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -17,3 +17,28 @@
 
 #----MIXIN VERSION---------------------------
 
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :(" 
+  end
+
+  def yell_happily(words)
+    words + "!" + " WOOHOO!!"
+  end 
+end 
+
+class Sibling
+  include Shout 
+end
+
+class Coworker
+  include Shout 
+end
+
+brother = Sibling.new 
+p brother.yell_angrily("Stop using my crayons")
+p brother.yell_happily("I got a new bike")
+
+cubicle_mate = Coworker.new 
+p cubicle_mate.yell_angrily("This place stinks") 
+p cubicle_mate.yell_happily("I quit") 
