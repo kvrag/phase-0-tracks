94bf7650c2d7dc605bf7f0178b4d5db37e70465e
Kristina Vragovic <k.vragovic@gmail.com>
2017-05-07 14:43:27 -0500

    Create Santa class and add basic methods

---
+++ ruby/santa.rb
@@ -0,0 +1,21 @@
+class Santa
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+
+  def initialize
+    puts "Initializing Santa instance..."
+  end 
+
+end
+
+# santa1 = Santa.new
+
+# santa1.speak
+
+# santa1.eat_milk_and_cookies("M&M cookie")
