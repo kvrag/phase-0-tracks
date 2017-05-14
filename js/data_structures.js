24c6a6edf585463b07dd30df48524d0cfd49c923
Kristina Vragovic <k.vragovic@gmail.com>
2017-05-14 13:53:38 -0500

    Create constructor function for car

--- js/data_structures.js
+++ js/data_structures.js
@@ -26,3 +26,24 @@ for (var i=0; i < names.length; i++) {
 }
 
 console.log(rainbow_horses)
+
+// write a constructor function for a car
+
+function Car(model, color) {
+  console.log("New car:", this)
+  this.model = model;
+  this.color = color; 
+  this.age = 0;
+  this.superCharged = true;
+    this.vroom = function() {
+      if (this.superCharged) {
+        console.log("Vvvvrrrrrrooooooooooom!");
+      } else {
+        console.log("*sputters, dies*");
+      }
+    };
+};
+
+var firstCar = new Car("Porsche", "sporty red")
+console.log(firstCar)
+console.log(firstCar.vroom)
