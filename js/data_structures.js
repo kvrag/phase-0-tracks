a812d6064e67922e78328842fc7b077ae3fed7c4
Kristina Vragovic <k.vragovic@gmail.com>
2017-05-14 13:02:24 -0500

    Initial commit

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["green", "pink", "blue", "yellow"]
+
+var names = ["Ed", "Murray", "Charlie", "Brownie"]
+
+colors.push("orange")
+console.log(colors)
+
+names.push("Stardust")
+console.log(names)
