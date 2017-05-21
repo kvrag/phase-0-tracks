20a63048e41b7c55d3c7add345a1f21d6e46df88
Kristina Vragovic <k.vragovic@gmail.com>
2017-05-21 16:31:37 -0500

    Define method for creating a garden

--- databases/something_awesome/garden_planner.rb
+++ databases/something_awesome/garden_planner.rb
@@ -3,7 +3,34 @@
 # - creates
 # - prints
 # - stores
-# their new garden data.
+# their new garden data
+
+# BUSINESS LOGIC ---------------------------
 
 require 'sqlite3'
-require 'garden.db'
+
+db = SQLite3::Database.new('garden.db')
+db.results_as_hash = true 
+
+
+garden = <<-SQL
+  CREATE TABLE IF NOT EXISTS garden_name(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+  )
+SQL
+
+def create_garden(garden_name)
+  db.execute(garden)
+end 
+
+
+
+
+# USER INTERFACE ---------------------------
+
+
+# DRIVER CODE ------------------------------
+
+# puts db.execute("SELECT * FROM vegetables;") 
